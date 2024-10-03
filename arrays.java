5235263a51b11c3c0245f3dd99c83b9b85827fae
Aditya-kori <16adityakori@gmail.com>
2024-10-03 11:54:08 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,21 @@
+public class arrays{
+	public static void main(String[] args){
+	byte[] costOfChocolates = {50,100,85,65,20,10,55,70,90,120};
+	System.out.println(costOfChocolates[4]);
+	short[] noOfWordsInEachSentence = {7452,8297,21940,4337,12736,1528,5738,23527,13384,2480};
+	System.out.println(noOfWordsInEachSentence[6]);
+	int[] observedValues = {-18273,23846,-13132,11234,3252,76573,236272,547346,23356,267732};
+	System.out.println(observedValues[2]);
+	long[] pinCodeCollection = {584103l,564193l,584101l,52371l,65712l,14432l,62345l,23226l,54573l,57332l};
+	System.out.println(pinCodeCollection[8]);
+	float[] cgpaCalculation = {9.14f,6.24f,8.36f,9.14f,6.44f,5.32f,6.25f,6.45f,8.45f,9.32f};
+	
+	double[] executionSpeed = {234.342131d,124.12432d,134.4352d,134.341214d,243.2314d,1.24334d,323.12342d,234.234223d,45.2534d,4.2341d};
+	System.out.println(cgpaCalculation[6]);
+	System.out.println(executionSpeed[8]);
+	char[] consanantsCollection = {'B','C','D','F','G','H','J','k','L','M'};
+	System.out.println(consanantsCollection[0]);
+	boolean[] probabilityOfCoinShowingHeads = {true,false,false,true,false,true,false,false,true,true};
+	System.out.println(probabilityOfCoinShowingHeads[9]);
+	}
+}
